02e1539e69b22a5a9fc74a2e77cc3ecbc3831f2b
Matt Ray <github@mattray.dev>
2020-08-04 16:31:05 +1000

    Convert to a nexus_sync custom resource
    Signed-off-by: Matt Ray <github@mattray.dev>

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # nexus_sync
 
-Given a token and and a URL, sync the contents of a Nexus file repository into a directory.
+This cookbook provides both a custom resource and a default cookbook that will sync a Nexus endpoint to a local directory. The attributes used and the custom resource are demonstrated in the default recipe:
+
+```
+nexus_sync node['nexus_sync']['directory'] do
+  url node['nexus_sync']['url']
+  user node['nexus_sync']['user']
+  password node['nexus_sync']['password']
+  action :sync
+end
+```
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,42 +3,9 @@
 # Recipe:: default
 #
 
-require 'net/http'
-require 'openssl'
-require 'uri'
-require 'json'
-
-directory node['nexus_sync']['directory'] do
-  recursive true
-end
-
-# # get the list of artifacts
-# uri = URI.parse(node['nexus_sync']['url'])
-# request = Net::HTTP::Get.new(uri)
-
-# # request["Api-Token"] = node['nexus_sync']['token']
-# request.basic_auth(node['nexus_sync']['user'], node['nexus_sync']['password'])
-
-# req_options = {
-#                use_ssl: uri.scheme == 'https',
-#                verify_mode: OpenSSL::SSL::VERIFY_NONE,
-#               }
-
-# response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-#   http.request(request)
-# end
-
-command = "curl -k --user #{node['nexus_sync']['user']}:#{node['nexus_sync']['password']} -X GET #{node['nexus_sync']['url']}"
-cmd = shell_out(command)
-downloads = JSON.parse(cmd.stdout)
-
-# iterate over response, downloading anything that doesn't already exist
-downloads['items'].each do |url|
-  filename = url['path'].split('/')[1]
-  remote_file "#{node['nexus_sync']['directory']}/#{filename}" do
-    source "https://#{node['nexus_sync']['user']}:#{node['nexus_sync']['password']}@#{url['downloadUrl'].sub('https://','')}"
-    # remote_user node['nexus_sync']['user']
-    # remote_password node['nexus_sync']['password']
-    # checksum url['checksum']['sha256']
-  end
+nexus_sync node['nexus_sync']['directory'] do
+  url node['nexus_sync']['url']
+  user node['nexus_sync']['user']
+  password node['nexus_sync']['password']
+  action :sync
 end
